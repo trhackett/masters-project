bbeebebf1b1563cb2d154b0d5b6221a1a4faa243
Trevor <trhackett@ucla.edu>
2019-01-24 19:58:45 -0800

    organized code, compiles but doesn't work

--- smartpointers/pimpl/simulation.cpp
+++ smartpointers/pimpl/simulation.cpp
@@ -4,75 +4,65 @@
 #include <queue>
 #include <iostream>
 
+/* ============================================================================
+	Potential heuristic functions
+   ============================================================================ */ 
 double manhattenDistance(int row, int col, Simulation* sim) {
 	return abs(row - sim->getEndRow()) +
        abs(col - sim->getEndCol());
 }
 
 double avoidTraffic(int row, int col, Simulation* sim) {
-	return sim->getIntersections()[row][col]->getNumCars();
-
-
-	/*
-	int r = car.row;
-	int c = car.col;
-
-	  // car can always move either upDir or downDir
-	bool upDirOk = r > 0;
-	bool rightDirOk = c < sim.getMaxCols()-1;
-	bool downDirOk = r < sim.getMaxCols()-1;
-	bool leftDirOk = c > 0;
-
-	int minDirection;
-
-	  // if upDir isn't ok, downDir definitely is
-	if (upDirOk) {
-		minDirection = sim.getIntersections()[r-1][c];
-	} else {
-		minDirection = sim.getIntersections()[r+1][c];
-	}
-
-	if (rightDirOk) {
-		int rightDirSize = sim.getIntersections()[r][c+1]
-		if (minDirection > rightDirSize) {
-			minDirection = rightDirSize;
-		}
-	}
-
-	if (downDirOk) {
-		int downDirSize = sim.getIntersections()[r+1][c]
-		if (minDirection > downDirSize) {
-			minDirection = downDirSize;
-		}
-	}
-
-	if (leftDirOk) {
-		int leftDirSize = sim.getIntersections()[r][c-1]
-		if (minDirection > leftDirSize) {
-			minDirection = leftDirSize;
-		}
-	}
-
-	return minDirection;
-	*/
+	return sim->getIntersectionAt(row, col)->getNumCars();
 }
+
+
+void Car::init(int row, int col, double (*hFunc) (int, int, Simulation*))
+{
+	m_row = row;
+	m_col = col;
+	m_heuristicFunc = hFunc;
+	m_timer.start();
+}
+
+double Car::getHValue(int row, int col, Simulation* sim) {
+	return m_heuristicFunc(row, col, sim);
+}
+
+
+  // cars leave intersections
+void Intersection::removeCar(SmartPtr<Car, SharedPtr> carPtr) {
+	set<SmartPtr<Car, SharedPtr>>::iterator it = cars.find(carPtr);
+
+	  // if it's in there, remove it
+	if (it != cars.end()) {
+		cars.erase(it);
+	}
+}
+
+  // and cars come into intersections
+void Intersection::addCar(SmartPtr<Car, SharedPtr> carPtr) {
+	cars.insert(carPtr);
+}
+
 
 Simulation::Simulation(int rows, int cols)
  : maxRows(rows), maxCols(cols), startRow(0), startCol(0),
    endRow(maxRows-1), endCol(maxCols-1), carFrequency(5)
 {
-	  // intersections is currently empty - we need to set upDir the
+	  // intersections is currently empty - we need to set Up the
 	  // environment by creating a bunch of intersection with their
 	  // delete functions
 	for (int i = 0; i < maxRows; i++) {
-		intersections.push_back(
+		iGrid.push_back(
 			vector<SmartPtr<Intersection, UniquePtr>>(maxCols)
 		);
 	}
 
 	for (int i = 0; i < maxRows; i++) {
 		for (int j = 0; j < maxCols; j++) {
-			intersections[i][j].reset(new Intersection,
+			iGrid[i][j].reset(
+				new Intersection,
 				[] (Intersection* i) { delete i; });
 		}
 	}
@@ -95,7 +85,8 @@ void Simulation::runSimulation(int numIterations) {
 		list<SmartPtr<Car, SharedPtr>>::iterator s = allCars.begin();
 		list<SmartPtr<Car, SharedPtr>>::iterator e = allCars.end();
 		for (; s != e; ++s) {
-			moveCarInDirection(*s, getNextMove(*s));
+			RouteComputer rc;
+			moveCarInDirection(*s, rc.getNextMove(*s, *this));
 		}
 	}
 }
@@ -106,73 +97,89 @@ void Simulation::insertNewCar() {
 		[] () { return new Car; }, 
 		[] (Car* c) { delete c; });
 
-	temp->row = startRow;
-	temp->col = startCol;
-
-	  // VARY THIS
-	temp->hFunc = &manhattenDistance;
+	  // to test out different heuristics, pass different
+	  // functions into here
+	temp->init(startRow, startCol, &manhattenDistance);
 
 	  // allCars needs to track it for movement
 	allCars.push_back(temp);
 
 	  // it is located in an intersection
-	intersections[startRow][startCol]->cars.insert(temp);
-}
-
-int Simulation::getRowInDirection(int row, Direction d) const {
-	  // left or right is ok
-	if (d == rightDir || d == leftDir) {
-		return row;
-	}
-
-	if (d == upDir && row > 0) {
-		return row-1;
-	}
-
-	if (d == downDir && row < maxRows-1) {
-		return row+1;
-	}
-
-	return -1;
-}
-int Simulation::getColInDirection(int col, Direction d) const {
-	  // up or down is ok
-	if (d == upDir || d == downDir) {
-		return col;
-	}
-
-	if (d == Direction::rightDir && col < maxCols-1) {
-		return col+1;
-	}
-
-	if (d == Direction::leftDir && col > 0) {
-		return col-1;
-	}
-
-	return -1;
+	iGrid[startRow][startCol]->addCar(temp);
 }
 
 
   // going into this function, you KNOW that the direction is a valid
   // one or it wouldn't even have been considered by getNextMove()
-void Simulation::moveCarInDirection(SmartPtr<Car, SharedPtr> carPtr, Direction d) {
+void Simulation::moveCarInDirection(SmartPtr<Car, SharedPtr> carPtr, Direction d)
+{
+	int currRow = carPtr->row();
+	int currCol = carPtr->col();
+	int nextRow = currRow;
+	int nextCol = currCol;
 
-	  // direction MUST be valid or else it wouldn't have been chosen
-	int currRow = carPtr->row;
-	int currCol = carPtr->col;
-	int nextRow = getRowInDirection(currRow, d);
-	int nextCol = getColInDirection(currCol, d);
-
-	  // I guess I'll check anyway
-	if (nextRow != -1 && nextCol != -1) {
-		intersections[nextRow][nextCol]->addCar(carPtr);
-		intersections[currRow][currCol]->removeCar(carPtr);
+	  // if you can move in that direction, then remove the car from the intersection
+	  // you're at currently and insert yourself at the one you're going to
+	if (getRowColInDirection(nextRow, nextCol, d)) {
+		iGrid[nextRow][nextCol]->addCar(carPtr);
+		iGrid[currRow][currCol]->removeCar(carPtr);
 	}
 }
 
 
+/*
+	This function takes in a row and column by reference that corresponds
+	to the actual position of a car as well as a direction that car would
+	like to see if it can move in. It checks to see if that move will take
+	the car out of bounds. If so, it leaves row and col unchanged and returns
+	false. If not, it adjusts row or col to reflect that move and returns
+	true.
+*/
+bool Simulation::getRowColInDirection(int& row, int& col, Direction d) const
+{
+	  // assume the move is invalid and then check to see if it results
+	  // in going out of bounds. If it doesn't, then adjust the row/col
+	  // and return true
+	bool validMove = false;
+	switch (d) {
+		case Up:
+			if (row > 0) {
+				row--;
+				validMove = true;
+			}
+			break;
+
+		case Right:
+			if (col < maxCols - 1) {
+				col++;
+				validMove = true;
+			}
+			break;
+
+		case Down:
+			if (row < maxRows - 1) {
+				row++;
+				validMove = true;
+			}
+			break;
+
+		case Left:
+			if (col > 0) {
+				col--;
+				validMove = true;
+			}
+			break;
+
+		case Bad:
+			break;
+	}
+
+	return validMove;
+}
+
+
   // this is where A* happens
-Direction Simulation::getNextMove(SmartPtr<Car, SharedPtr> car) {
+Direction RouteComputer::getNextMove(SmartPtr<Car, SharedPtr> car, Simulation& sim) {
 
 	  // init empty
 	priority_queue<node, vector<node>, decltype(cmpNodes)> openList(cmpNodes);
@@ -181,36 +188,47 @@ Direction Simulation::getNextMove(SmartPtr<Car, SharedPtr> car) {
 	map<pair<int, int>, double> openFVals;
 
 	  // starting point goes in open list, zero f
-	openList.push(node(car->row, car->col, -1, -1, 0, 0));
+	openList.push(node(car->row(), car->col(), -1, -1, 0, 0));
 
 	while (!openList.empty()) {
+
 		  // get the node with the smallest f and pop off open list
 		node parent = openList.top();
 		openList.pop();
 
+		  // if you're at the end, be done!
+		if (parent.row == sim.getEndRow() && parent.col == sim.getEndCol()) {
+			return backtrack(parent.row, parent.col,
+						     car->row(), car->col(), closedList);
+		}
+
+		  // also remove it from the open f values
+		pair<int, int> p(parent.row, parent.col);
+		map<pair<int, int>, double>::iterator it = openFVals.find(p);
+		if (it != openFVals.end()) {
+			openFVals.erase(it);
+		}
+
 		  // try each direction
 		for (int i = 0; i < 4; i++) {
-			int nextRow = getRowInDirection(parent.row, (Direction)i);
-			int nextCol = getColInDirection(parent.col, (Direction)i);
+			int nextRow = parent.row;
+			int nextCol = parent.col;
+			Direction d = (Direction) i;
 
-			  // if it's a valid move, add it in
-			if (nextRow != -1 && nextCol != -1) {
+			  // try up
+			if (sim.getRowColInDirection(nextRow, nextCol, d)) {
 
-				  // if you're there, stop
-				if (nextRow == endRow && nextCol == endCol) {
-					return backtrack(parent.row, parent.col, closedList);
-				}
-
-				  // make the new node
-				double hVal = car->hFunc(nextRow, nextCol, this);
+				  // add it to the open list as long as there isn't an f value
+				  // corresponding to that point on the grid that is smaller than
+				  // this one
+				double hVal = car->getHValue(nextRow, nextCol, &sim);
 				node child(nextRow, nextCol, parent.row, parent.col, parent.g + 1, hVal);
 
-				  // add it to the list as long as there isn't already
-				  // a cheaper way to get there
 				if (!betterOptionIn(child.row, child.col, child.f, openFVals) &&
 					!betterOptionIn(child.row, child.col, child.f, closedFVals))
 				{
 					openList.push(child);
+					openFVals[pair<int, int>(child.row, child.col)] = child.f;
 				}
 			}
 		}
@@ -220,32 +238,36 @@ Direction Simulation::getNextMove(SmartPtr<Car, SharedPtr> car) {
 		closedFVals[pair<int, int>(parent.row, parent.col)] = parent.f;
 	}
 
-	return upDir;
+	return Bad;
 }
 
-Direction Simulation::evalDirection(int fromR, int fromC, int toR, int toC) {
+Direction RouteComputer::evalDirection(int fromR, int fromC,
+										 int toR, int toC)
+{
 	  // coming from row above, going down
 	if (toR == fromR + 1) {
-		return downDir;
+		return Down;
 	}
 
 	if (toR == fromR - 1) {
-		return upDir;
+		return Up;
 	}
 
 	  // coming from left, going right
 	if (toC == fromC + 1) {
-		return rightDir;
+		return Right;
 	}
 
 	if (toC == fromC - 1) {
-		return leftDir;
+		return Left;
 	}
 
-	return badDir;
+	return Bad;
 }
 
-Direction Simulation::backtrack(int r, int c, map<pair<int, int>, node>& closedList) {
+Direction RouteComputer::backtrack(int r, int c, int startRow, int startCol,
+									 map<pair<int, int>, node>& closedList)
+{
 	int parentRow = closedList[pair<int, int>(r, c)].parentRow;
 	int parentCol = closedList[pair<int, int>(r, c)].parentCol;
 
@@ -255,10 +277,12 @@ Direction Simulation::backtrack(int r, int c, map<pair<int, int>, node>& closedL
 	}
 
 	  // if not, then keep following the parent route
-	return backtrack(parentRow, parentCol, closedList);
+	return backtrack(parentRow, parentCol, startRow, startCol, closedList);
 }
 
-bool Simulation::betterOptionIn(int r, int c, double f, map<pair<int, int>, double>& fVals) {
+bool RouteComputer::betterOptionIn(int r, int c, double f,
+									 map<pair<int, int>, double>& fVals)
+{
 	map<pair<int, int>, double>::iterator it = fVals.find(pair<int, int>(r, c));
 
 	  // if there's an f value in there and it's better than the one we have, return
--- smartpointers/pimpl/simulation.h
+++ smartpointers/pimpl/simulation.h
@@ -13,63 +13,125 @@
 using namespace std;
 
   // potentially four directions a car can move in
-enum Direction { upDir, rightDir, downDir, leftDir, badDir };
+enum Direction { Up, Right, Down, Left, Bad };
+
+class Simulation;
+
+/* ============================================================================
+	Cars can have individual heuristic functions and each has their
+	own position and timer running
+   ============================================================================ */
+class Car {
+public:
+	Car() { }
+
+	void init(int row, int col, double (*hFunc) (int, int, Simulation*));
+
+	  // basic get/set functions
+	void restartTime()    { m_timer.start(); }
+	int getTime() const   { return m_timer.elapsed(); }
+	int row() const       { return m_row; }
+	int col() const       { return m_col; }
+
+	  // calling the heuristic function
+	double getHValue(int row, int col, Simulation* sim);
+
+private:
+	Timer m_timer;
+	int m_row, m_col;
+	double (*m_heuristicFunc) (int, int, Simulation*);
+};
+
+
+/* ============================================================================
+	Each simulation has a grid of Intersections that has a list of cars which will
+	change as the simulation progresses.
+
+	There can be any number of cars at an intersection at any given time but a car
+	can be at only one intersection at any given time.
+   ============================================================================ */
+class Intersection {
+public:
+	Intersection() { }
+
+	  // get the number of cars at the intersection (may want more data points)
+	  // to support more heuristics, that's fine
+	int getNumCars() const  { return cars.size(); }
+
+	  // make adjustments to the set of cars here
+	void removeCar(SmartPtr<Car, SharedPtr> carPtr);
+	void addCar(SmartPtr<Car, SharedPtr> carPtr);
+
+
+private:
+	set<SmartPtr<Car, SharedPtr>> cars;
+};
+
+
+  // stored as a two dimensional array of smart pointers to an
+  // intersection
+using IntersectionGrid = vector<vector<SmartPtr<Intersection, UniquePtr>>>;
+
+/* ============================================================================
+	Route Computer
+   ============================================================================ */
+
+class RouteComputer {
+public:
+	  // when getting the next move, you have complete knowledge
+	  // of the simulation at that time - you know how many cars
+	  // are at each intersection and you also know the goal
+	Direction getNextMove(SmartPtr<Car, SharedPtr> carPtr, Simulation& sim);
+
+private:
+	  // node struct and comparison function only for A*
+	struct node {
+		int row, col;
+		int parentRow, parentCol;
+		double g, h, f;
+		node(int r, int c, int pr, int pc, double gVal, double hVal)
+		 : row(r), col(c), parentRow(pr), parentCol(pc), g(gVal), h(hVal), f(g+h)
+		{ }
+		node() {}
+	};
+
+	  // comparison function that allows us to use priority queue with
+	  // nodes - this is what ensures that the lowest f values are
+	  // popped off first
+	bool (*cmpNodes) (const node&, const node&) =
+		[] (const node& left, const node& right)
+		{ return left.f > right.f; };
+
+	Direction backtrack(int r, int c, int startRow, int startCol,
+					    map<pair<int, int>, node>& closedList);
+	bool betterOptionIn(int r, int c, double f, map<pair<int, int>, double>& fVals);
+	Direction evalDirection(int fromR, int fromC, int toR, int toC);
+};
+
+
+
+/* ============================================================================
+	Meat and Bones
+   ============================================================================ */
 
 class Simulation {
 public:
 	Simulation(int r, int c);
 	void runSimulation(int numIterations);
 
-	  // the only thing that cars need to know about are its
-	  // position and it's routing function
-	struct Car {
-		Timer t;
-		int row, col;
-		  // heuristic function used by A*
-		double (*hFunc) (int, int, Simulation*);
-	};
-	  // the road is simplified to a 2D grid of intersections.
-	  // There can be any number of cars at an intersection at
-	  // any given time but each car can only be at one intersection
-	  // at any given time. Each time step, every car gets to move
-	  // to the next intersection - provided by it routing function
-	struct Intersection {
-		set<SmartPtr<Car, SharedPtr>> cars;
-		int getNumCars() const { return cars.size(); }
-
-		  // cars leave intersections
-		void removeCar(SmartPtr<Car, SharedPtr> carPtr) {
-			set<SmartPtr<Car, SharedPtr>>::iterator it = cars.find(carPtr);
-
-			  // if it's in there, remove it
-			if (it != cars.end()) {
-				cars.erase(it);
-			}
-		}
-
-		  // and cars come into intersections
-		void addCar(SmartPtr<Car, SharedPtr> carPtr) {
-			cars.insert(carPtr);
-		}
-	};
-
-	int getEndRow() const { return endRow; }
-	int getEndCol() const { return endCol; }
+	int getEndRow() const  { return endRow; }
+	int getEndCol() const  { return endCol; }
 	int getMaxRows() const { return maxRows; }
 	int getMaxCols() const { return maxCols; }
 	  // must return by const reference to obey the promise the const function makes
-	const vector<vector<SmartPtr<Intersection, UniquePtr>>>& getIntersections() const {
-		return intersections;
-	}
+	SmartPtr<Intersection, UniquePtr> getIntersectionAt(int r, int c) const
+		{ return iGrid[r][c]; }
 
-	int getRowInDirection(int, Direction) const;
-	int getColInDirection(int, Direction) const;
+	bool getRowColInDirection(int& row, int& col, Direction d) const;
 
 private:
-	  // stored as a two dimensional array of smart pointers to an
-	  // intersection
-	vector< vector< SmartPtr<Intersection, UniquePtr> > > intersections;
-	// Intersection** intersections;
+	  // 2D array (vector) of Intersections
+	IntersectionGrid iGrid;
 
 	int maxRows, maxCols;
 	  // the simulation has an index at which cars enter and an index at which
@@ -85,30 +147,7 @@ private:
 	  // first iteration
 	void insertNewCar();
 	  // takes a car and a direction and moves it in that direction
-	void moveCarInDirection(SmartPtr<Car, SharedPtr>, Direction);
-	  // when getting the next move, you have complete knowledge
-	  // of the simulation at that time - you know how many cars
-	  // are at each intersection and you also know the goal
-	Direction getNextMove(SmartPtr<Car, SharedPtr>);
-
-	  // node struct and comparison function only for A*
-	struct node {
-		int row, col;
-		int parentRow, parentCol;
-		double g, h, f;
-		node(int r, int c, int pr, int pc, double gVal, double hVal)
-		 : row(r), col(c), parentRow(pr), parentCol(pc), g(gVal), h(hVal), f(g+h)
-		{ }
-		node() {}
-	};
-
-	bool (*cmpNodes) (const node&, const node&) =
-		[] (const node& left, const node& right)
-		{ return left.f > right.f; };
-
-	Direction backtrack(int r, int c, map<pair<int, int>, node>& closedList);
-	bool betterOptionIn(int r, int c, double f, map<pair<int, int>, double>& fVals);
-	Direction evalDirection(int fromR, int fromC, int toR, int toC);
+	void moveCarInDirection(SmartPtr<Car, SharedPtr> carPtr, Direction d);
 };
 
 #endif
